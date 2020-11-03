ad426f8563d41ab86b93e0a72cb28b75fde162eb
Avery Brown-Gathings <72925420+averyjbrown2@users.noreply.github.com>
2020-11-02 21:49:13 -0500

    deleted extra files

--- script.js
+++ script.js
@@ -1,12 +1,18 @@
 
-    var specialChar = ["!", "@", "#", "$"]
-    var lowercase = ["a" "b", "c", "d"]
-    var uppercase = ["A", "B", "C"]
-    var numeric = ["1", "2", "3"]
+    var specialChar = ["!", "@", "#", "$", "%", 
+                        "^", "&", "*","(", ")", 
+                        "-", "_", "+", "=", "?"]
+    var lowercase = ["a", "b", "c", "d", "e", "f", "g", "h",
+                    "i", "j", "k", "l", "m", "n", "o", "p", "q", "r",
+                    "s", "t", "u", "v", "w", "x", "y", "z"]
+    var uppercase = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J",
+                    "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", 
+                    "U", "V", "W", "X", "Y", "Z"]
+    var numeric = ["O", "1", "2", "3", "4", "5", "6", "7", "8", "9"]
     
 
     function characterCheck (){
-        var lengthCheck = parseINT(prompt("How many characters would you like your password to be?"));
+        var lengthCheck = parseINT(prompt("How many characters would you like your password to be, between 8 and 128?"));
         if (isNaN(lengthCheck)) {
             alert("You have not input a number");
             return;
@@ -36,9 +42,30 @@
         return confirmVariables; 
         }
 
+
+      
+        google how to get function to get element from Array
         
-        
+         = ""
+        for (var i = 0, i < lengthCheck; i++) {
+           console.log(Math.floor(Math.random)) *
+            pullSpecial += Math.floor(Math.random)  * (specialChar.length)
+        function to make password based on user Input
+
     
+        var generateBtn = document.querySelector("#generate");
+
+// Write password to the #password input
+function writePassword() {
+  var password = generatePassword(); makes password based on user input
+  var passwordText = document.querySelector("#password");
+
+  passwordText.value = password;
+
+}
+
+// Add event listener to generate button
+generateBtn.addEventListener("click", writePassword);
 
 
 

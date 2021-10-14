12511d40fe586f021921d620f0db23ba56ac67d9
Avery Brown-Gathings <72925420+averyjbrown2@users.noreply.github.com>
2021-10-14 13:10:04 -0400

    created password criteria prompts

--- Assets/script1.js
+++ Assets/script1.js
@@ -2,8 +2,15 @@
 var generateBtn = document.querySelector("#generate");
 
 ///write functions to ask questions
+///Choose length of password
 function generatePassword() {
     let NumChar = prompt("Choose a password length between 8 and 128 characters");
+    let lowerCaseLetters = prompt("Do you want lowercase letters?");
+    let upperCaseLetters = prompt("Do you want uppercase letters?");
+    let numeric = prompt("Do you want numeric characters?");
+    let specialChar = prompt("Do you want special characters?");
+    
+
 }
 
 // Write password to the #password input
